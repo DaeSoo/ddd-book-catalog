3c6ef1277b6bba6ca7d7cdf29be95f599c0cd38e
DaeSoo <qkajek@gmail.com>
2023-11-13 18:04:00 +0900

    Initial version of bookCatalog generated by generator-jhipster@8.0.0

---
+++ src/test/java/com/daesoo/bookcatalog/cucumber/CucumberIT.java
@@ -0,0 +1,8 @@
+package com.daesoo.bookcatalog.cucumber;
+
+import com.daesoo.bookcatalog.IntegrationTest;
+import io.cucumber.junit.platform.engine.Cucumber;
+
+@Cucumber
+@IntegrationTest
+class CucumberIT {}
---
+++ src/test/java/com/daesoo/bookcatalog/cucumber/CucumberTestContextConfiguration.java
@@ -0,0 +1,10 @@
+package com.daesoo.bookcatalog.cucumber;
+
+import com.daesoo.bookcatalog.IntegrationTest;
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@CucumberContextConfiguration
+@IntegrationTest
+@WebAppConfiguration
+public class CucumberTestContextConfiguration {}
